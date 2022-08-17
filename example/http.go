6203e8f3a39d75056fbde040dca76a0282a70c11
Josh Robson Chase <joshrc@ngrok.com>
2022-08-17 18:23:34 -0400

    update http example again

--- example/http.go
+++ example/http.go
@@ -6,8 +6,8 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"net"
 	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/davecgh/go-spew/spew"
@@ -22,9 +22,15 @@ func exitErr(err error) {
 }
 func main() {
 	ctx := context.Background()
+
+	proxyURL, err := url.Parse("socks5://localhost:1080")
+	exitErr(err)
+
 	opts := libngrok.ConnectOptions().
 		WithAuthToken(os.Getenv("NGROK_TOKEN")).
-		WithServer(os.Getenv("NGROK_SERVER"))
+		WithServer(os.Getenv("NGROK_SERVER")).
+		WithMetadata("Hello, world!").
+		WithProxyURL(proxyURL)
 	if caPath := os.Getenv("NGROK_CA"); caPath != "" {
 		caBytes, err := ioutil.ReadFile(caPath)
 		exitErr(err)
@@ -36,67 +42,22 @@ func main() {
 		opts.WithCA(pool)
 	}
 	sess, err := libngrok.Connect(ctx, opts)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	tun, err := sess.StartTunnel(ctx, libngrok.HTTPOptions().
-		WithOAuth(libngrok.OAuthProvider("google").
-			AllowDomain("ngrok.com"),
-		),
+	exitErr(err)
+
+	tun, err := sess.StartTunnel(ctx, libngrok.
+		HTTPOptions().
+		WithMetadata(`{"foo":"bar"}`).
+		WithForwardsTo("foobarbaz"),
 	)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	l := tun.AsTCP()
+	l := tun.AsHTTP()
 	fmt.Println("url: ", l.URL())
 
-	err = http.Serve(&dumpListener{l}, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	err = l.Serve(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		spew.Fdump(w, r)
 	}))
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-}
-
-type dumpListener struct {
-	inner net.Listener
-}
-
-type dumpConn struct {
-	net.Conn
-}
-
-func (dc *dumpConn) Read(bs []byte) (int, error) {
-	n, err := dc.Conn.Read(bs)
-
-	spew.Dump(bs[:n])
-
-	return n, err
-}
-
-// Accept waits for and returns the next connection to the listener.
-func (dl *dumpListener) Accept() (net.Conn, error) {
-	inner, err := dl.inner.Accept()
-	if err != nil {
-		return nil, err
-	}
-
-	fmt.Println("accepted connection from", inner.RemoteAddr())
-
-	return &dumpConn{inner}, nil
-}
-
-// Close closes the listener.
-// Any blocked Accept operations will be unblocked and return errors.
-func (dl *dumpListener) Close() error {
-	return dl.inner.Close()
-}
-
-// Addr returns the listener's network address.
-func (dl *dumpListener) Addr() net.Addr {
-	return dl.inner.Addr()
+	exitErr(err)
 }
