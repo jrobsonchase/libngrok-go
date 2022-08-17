a305a2e0579888fe0aebc4a41315dcca25b33e3c
Josh Robson Chase <joshrc@ngrok.com>
2022-08-17 14:43:25 -0400

    update http example

--- example/http.go
+++ example/http.go
@@ -2,36 +2,101 @@ package main
 
 import (
 	"context"
+	"crypto/x509"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"net"
 	"net/http"
 	"os"
 
+	"github.com/davecgh/go-spew/spew"
 	libngrok "github.com/ngrok/libngrok-go"
 )
 
+func exitErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
 func main() {
 	ctx := context.Background()
-	sess, err := libngrok.Connect(ctx, libngrok.
-		ConnectOptions().
-		WithAuthToken(os.Getenv("NGROK_TOKEN")),
+	opts := libngrok.ConnectOptions().
+		WithAuthToken(os.Getenv("NGROK_TOKEN")).
+		WithServer(os.Getenv("NGROK_SERVER"))
+	if caPath := os.Getenv("NGROK_CA"); caPath != "" {
+		caBytes, err := ioutil.ReadFile(caPath)
+		exitErr(err)
+		pool := x509.NewCertPool()
+		ok := pool.AppendCertsFromPEM(caBytes)
+		if !ok {
+			exitErr(errors.New("failed to add CA Certificates"))
+		}
+		opts.WithCA(pool)
+	}
+	sess, err := libngrok.Connect(ctx, opts)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	tun, err := sess.StartTunnel(ctx, libngrok.HTTPOptions().
+		WithOAuth(libngrok.OAuthProvider("google").
+			AllowDomain("ngrok.com"),
+		),
 	)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	tun, err := sess.StartTunnel(ctx, libngrok.
-		HTTPOptions().WithCompression(),
-	)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	fmt.Println("url: ", tun.URL())
-	err = http.Serve(tun, http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprintln(rw, "Hello, world!")
+	l := tun.AsTCP()
+	fmt.Println("url: ", l.URL())
+
+	err = http.Serve(&dumpListener{l}, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		spew.Fdump(w, r)
 	}))
+
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+type dumpListener struct {
+	inner net.Listener
+}
+
+type dumpConn struct {
+	net.Conn
+}
+
+func (dc *dumpConn) Read(bs []byte) (int, error) {
+	n, err := dc.Conn.Read(bs)
+
+	spew.Dump(bs[:n])
+
+	return n, err
+}
+
+// Accept waits for and returns the next connection to the listener.
+func (dl *dumpListener) Accept() (net.Conn, error) {
+	inner, err := dl.inner.Accept()
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("accepted connection from", inner.RemoteAddr())
+
+	return &dumpConn{inner}, nil
+}
+
+// Close closes the listener.
+// Any blocked Accept operations will be unblocked and return errors.
+func (dl *dumpListener) Close() error {
+	return dl.inner.Close()
+}
+
+// Addr returns the listener's network address.
+func (dl *dumpListener) Addr() net.Addr {
+	return dl.inner.Addr()
 }
--- go.mod
+++ go.mod
@@ -18,7 +18,9 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
-	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+	golang.org/x/net v0.0.0-20220805013720-a33c5aa5df48 // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
